d75c751de1c0b0f871c63c0e015d33df177bfbb5
Georgios Balatzis <159136767+balatzisgeorgios@users.noreply.github.com>
2024-02-06 13:51:32 +0200

    Updated index.ts

--- Fork1/src/index.ts
+++ Fork1/src/index.ts
@@ -4,11 +4,14 @@ type Dates = {
 };
 
 const isValidDate = (dateString: string): boolean => {
-  const dateRegex = /^\d{2}\.\d{2}.\d{4}$/;
+  // This regex matches dates in the format DD.MM.YYYY or DD/MM/YYYY or DD-MM-YYYY
+  // It also ensures that the day and month are within valid ranges and that the year starts with  19 or  20
+  const dateRegex = /^(0[1-9]|[12][0-9]|3[01])([.-/])(0[1-9]|1[012])([.-/])(19|20)\d{2}$/;
   return dateRegex.test(dateString);
 };
 
-const extractDates = (customer: { ListOfPeriods?: string }): Array<Dates | string> => {
+
+const toExtractDates = (customer: { ListOfPeriods?: string }): Array<Dates | string> => {
   if (!customer.ListOfPeriods) {
     return [];
   }
@@ -32,13 +35,11 @@ const extractDates = (customer: { ListOfPeriods?: string }): Array<Dates | strin
 };
 
 // Beispielaufrufe
-console.log(extractDates({ ListOfPeriods: '15.03.2017 - 01.05.2017' }));
+console.log(toExtractDates({ ListOfPeriods: '15.03.2017 - 01.05.2017' }));
 console.log(
-  extractDates({
+  toExtractDates({
     ListOfPeriods: '12.06.2008- 11.07.2008|12.08.2008 - 11.11.2008',
   })
 );
-console.log(extractDates({ ListOfPeriods: 'NULL' }));
-console.log(extractDates({}));
-
-
+console.log(toExtractDates({ ListOfPeriods: 'NULL' }));
+console.log(toExtractDates({}));
